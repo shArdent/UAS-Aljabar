9e2d59279fa9784f770ed0ac099f4f5eb939e7fc
NDIV123 <dmarshelano@gmail.com>
2024-11-28 12:36:44 +0700

    fix padding issue

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def reflect():
             }
             return jsonify(response)
 
-    return render_template('index.html')
+    return render_template('reflect.html')
 
 @app.route('/translate', methods=['GET', 'POST'])
 def translate():
